160bbfff86abdfe416a66d038963806505941c37
Silayuth <rockyjimslive@gmail.com>
2024-12-02 14:29:33 +0700

    com

--- server.js
+++ server.js
@@ -23,30 +23,34 @@ connectCloudinary();
 // middlewares
 app.use(express.json());
 app.use(cors());
+const corsOptions = {
+  origin: 'https://admin-app-cyan.vercel.app/',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+// const allowedOrigins = [
+//   "https://admin-app-cyan.vercel.app/",
+//   "http://localhost:5175", // For local development
+//   "http://localhost:5176", // For local development
+//   "http://localhost:5173"
+// ];
 
-const allowedOrigins = [
-  "https://admin-app-cyan.vercel.app/",
-  "http://localhost:5175", // For local development
-  "http://localhost:5176", // For local development
-  "http://localhost:5173"
-];
-
-// Configure CORS
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (e.g., mobile apps or curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true); // Allow the origin
-      } else {
-        callback(new Error("Not allowed by CORS")); // Block the origin
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // Allow cookies or Authorization headers
-  })
-);
+// // Configure CORS
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       // Allow requests with no origin (e.g., mobile apps or curl)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.includes(origin)) {
+//         callback(null, true); // Allow the origin
+//       } else {
+//         callback(new Error("Not allowed by CORS")); // Block the origin
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true, // Allow cookies or Authorization headers
+//   })
+// );
 
 // api endpoints
 app.use("/api/user", userRouter);
